fd08a6350d7fdaa27f5e088994212d451a168f03
rapme <rap-me-online@yandex.ru>
2020-09-19 17:21:32 +0500

    make bat

---
+++ main.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\Nikitos\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\Nikitos\Documents\rapme\fl-auto\main.py" %1 %2
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pyautogui
 import time
 import tkinter.messagebox
+import sys
 
 # Disable CapsLock Or NumLock Before start!!!
 # Close All Windows Except Piano Roll
 # Select D#2 note!!!
+# ./main.bat bass 2
 
 time.sleep(5)
+
+instrument = sys.argv[1]
+number = sys.argv[2]
+
 
 def exportNote(name):
     pyautogui.keyDown('shift')
@@ -27,7 +33,8 @@ def exportNote(name):
 
     time.sleep(1)
 
-    pyautogui.write(name)
+    pyautogui.write('C:\\Users\\Nikitos\\Documents\\rapme\\source\\api\\samples\\{}\\{}\\{}'.format(
+        instrument, number, note))
     time.sleep(1)
     pyautogui.hotkey('enter')
     time.sleep(1)
@@ -35,22 +42,38 @@ def exportNote(name):
 
     time.sleep(2)
 
-notes = ['E2.mp3',
-         'F2.mp3',
-         'Fs2.mp3',
-         'G2.mp3',
-         'Gs2.mp3',
-         'A2.mp3',
-         'As2.mp3',
-         'B2.mp3',
-         'C2.mp3',
-         'Cs2.mp3',
-         'D2.mp3',
-         'Ds2.mp3',
-         'E3.mp3']
+
+# notes = ['E2.mp3',
+#          'F2.mp3',
+#          'Fs2.mp3',
+#          'G2.mp3',
+#          'Gs2.mp3',
+#          'A2.mp3',
+#          'As2.mp3',
+#          'B2.mp3',
+#          'C2.mp3',
+#          'Cs2.mp3',
+#          'D2.mp3',
+#          'Ds2.mp3',
+#          'E3.mp3']
+
+#  For BASS
+notes = [
+    'A2.mp3',
+    'As2.mp3',
+    'B2.mp3',
+    'C2.mp3',
+    'Cs2.mp3',
+    'D2.mp3',
+    'Ds2.mp3',
+    'E2.mp3',
+    'F2.mp3',
+    'Fs2.mp3',
+    'G2.mp3',
+    'Gs2.mp3']
 
 for note in notes:
     exportNote(note)
     print(note)
 
-tkinter.messagebox.showinfo(title='Export Done!!!',message='Eeeee, done!')
+tkinter.messagebox.showinfo(title='Export Done!!!', message='Eeeee, done!')
